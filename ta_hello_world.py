8c58cef745af06bf20088283cf7b7ffe2cf6676c
Silas Berger <SilasBerger@users.noreply.github.com>
2025-05-26 13:34:47 +0200

    Create ta_hello_world.py
    Add hello world example for TA publication experiment.

---
+++ ta_hello_world.py
@@ -0,0 +1,12 @@
+entscheid = input('Willst du gehen oder bleiben? [gehen/bleiben]: ').lower()
+if entscheid == 'gehen':
+  print('Ok, ciao!')
+elif entscheid == 'bleiben':
+  print('Gute Wahl.)
+  entscheid = input('Willst du gewinnen oder verlieren? [gewinnen/verlieren]: ')
+  if entscheid == 'gewinnen':
+    print('Du hast gewonnen! 🥳')
+  else:
+    print('Du hast verloren! 😞')
+else:
+  print('Ungültige Wahl. Spiel vorbei!')
